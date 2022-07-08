951e34fac41b8167a82ad1e2f21cfd503f9e3a37
yannan xing <yannan.xing@synsense.ai>
2022-07-08 15:32:52 +0800

    add proxy learning part

--- Proxy-learning/nmnist/QAT-Proxy-addtograph.py
+++ Proxy-learning/nmnist/QAT-Proxy-addtograph.py
@@ -13,6 +13,7 @@ import copy
 from sinabs.backend.dynapcnn import DynapcnnCompatibleNetwork
 import torch.nn.functional as F
 from sinabs.layers import IAFSqueeze
+from copy import deepcopy
 
 epochs = 100
 batchsize = 500
@@ -84,8 +85,10 @@ class ANN(torch.nn.Module):
         self.pool2 = nn.AvgPool2d(kernel_size=(2, 2))
         self.relu = nn.ReLU()
         self.round = StochasticRounding
+        self.leakrelu = nn.LeakyReLU()
 
     def forward(self, sample):
+
         q1 = _quantization_scale(self.conv1.weight.data)
         propgate = self.relu(self.round.apply(self.conv1(sample) * q1) / q1)
         q2 = _quantization_scale(self.conv2.weight.data)
@@ -97,7 +100,7 @@ class ANN(torch.nn.Module):
         q4 = _quantization_scale(self.fc1.weight.data)
         propgate = self.relu(self.round.apply(self.fc1(propgate) * q4) / q4)
         q5 = _quantization_scale(self.fc2.weight.data)
-        propgate = self.round.apply(self.fc2(propgate) * q5) / q5
+        propgate = self.leakrelu(self.round.apply(self.fc2(propgate) * q5) / q5)
 
         return propgate
 
@@ -128,11 +131,14 @@ class SNN(torch.nn.Module):
                        reset_fn=MembraneSubtract(), spike_threshold=1, min_v_mem=-1),
 
             nn.Linear(256, 10, bias=False),
+            IAFSqueeze(batch_size=batchsize, surrogate_grad_fn=PeriodicExponential(),
+                       reset_fn=MembraneSubtract(), spike_threshold=1, min_v_mem=-1),
         )
 
     def forward(self, sample):
-        sample = sample.clone().sum(2).unsqueeze(2)
-        sample = sample.to(self.device)
+
+        # sample = sample.clone().sum(2).unsqueeze(2)
+        # sample = sample.to(self.device)
         (batch_size, t_len, channel, height, width) = sample.shape
         sample = sample.reshape((batch_size * t_len, channel, height, width))
         out = self.snn(sample)
@@ -144,6 +150,11 @@ class SNN(torch.nn.Module):
         for lyrs in self.snn:
             if isinstance(lyrs, IAFSqueeze):
                 lyrs.v_mem.detach_()
+
+    def reset_state(self):
+        for lyrs in self.snn:
+            if isinstance(lyrs, IAFSqueeze):
+                lyrs.reset_states(randomize=True)
 
 
 ann_seq = nn.Sequential(
@@ -166,12 +177,44 @@ ann_seq = nn.Sequential(
 )
 
 
-def weight_transfer(module: nn.Module, seq: nn.Sequential):
-    seq[0].weight.data = torch.round(module.conv1.weight.data.clone() * _quantization_scale(module.conv1.weight.data))
-    seq[2].weight.data = torch.round(module.conv2.weight.data.clone() * _quantization_scale(module.conv2.weight.data))
-    seq[5].weight.data = torch.round(module.conv3.weight.data.clone() * _quantization_scale(module.conv3.weight.data))
-    seq[9].weight.data = torch.round(module.fc1.weight.data.clone() * _quantization_scale(module.fc1.weight.data))
-    seq[11].weight.data = torch.round(module.fc2.weight.data.clone() * _quantization_scale(module.fc2.weight.data))
+def _discrete_snn(mode: nn.Module, w_precision=8, state_precision=16, min_vmem=-1, spike_threshold=1):
+    """
+    Does the identical quantization method with DYNAPCNN-COMPATIBLE network discrete method
+    :param model: sequential snn model, suppose to have IAFsqueeze layers followed by parameter layers
+    :param w_precision: predefined by chip
+    :param state_precision:  predefined by chip
+    :return:
+    """
+
+    depth = len(mode)
+
+    for i, lyrs in enumerate(mode):
+        # if i <= depth - 2:
+        if isinstance(lyrs, nn.Conv2d) or isinstance(lyrs, nn.Linear):
+            # print(f"current_layer numner: {i}")
+            w_factor = _quantization_scale(lyrs.weight.data, bit_precision=w_precision)
+            thresholds = torch.tensor((min_vmem, spike_threshold)).to(device)
+            t_factor = _quantization_scale(thresholds, bit_precision=state_precision)
+            scale = min(w_factor, t_factor).to(device)
+
+            lyrs.weight.data = torch.round(lyrs.weight.data.clone() * scale).to(device)
+            (mode[i + 1].min_v_mem, mode[i + 1].spike_threshold) = torch.round(thresholds.clone() * scale).to(device)
+            mode[i + 1].v_mem.data = torch.round(mode[i + 1].v_mem.data.clone() * scale).to(device)
+        # else:
+        #     if isinstance(lyrs, nn.Conv2d) or isinstance(lyrs, nn.Linear):
+        #         # print(f"current_layer numner: {i}")
+        #         w_factor = _quantization_scale(lyrs.weight.data, bit_precision=w_precision)
+        #         lyrs.weight.data = torch.round(lyrs.weight.data.clone() * w_factor).to(device)
+
+
+def weight_sharing(anns: nn.Module, snns: nn.Module):
+    # convert fp weight from ann to snn and quantization occurs on snn side
+    snns.snn[0].weight.data = anns.conv1.weight.data.clone()
+    snns.snn[2].weight.data = anns.conv2.weight.data.clone()
+    snns.snn[5].weight.data = anns.conv3.weight.data.clone()
+    snns.snn[9].weight.data = anns.fc1.weight.data.clone()
+    snns.snn[11].weight.data = anns.fc2.weight.data.clone()
+
 
 
 
@@ -186,63 +229,102 @@ test_loader = DataLoader(test_raster, batch_size=batchsize, shuffle=True, drop_l
 
 # model = ANN_SNN_PROXY_MODULE(batch_size=batchsize, device=torch.device("cuda"))
 device = torch.device("cuda")
-model = ANN()
-model.to(device)
+ann = ANN()
+ann.to(device)
 ann_seq = ann_seq.to(device)
+snn = SNN(batchsize).to(device)
+
+
 # for _ in range(1000):
 #     out = model.ann_forward(torch.ones((500, 1, 1, 34, 34)))
 #     print(out.sum().sum())
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+optimizer = torch.optim.Adam(ann.parameters(), lr=1e-4, weight_decay=1e-8)
 criterion = nn.CrossEntropyLoss()
 
-for m in model.children():
+for m in ann.children():
     if isinstance(m, (nn.Conv2d, nn.Linear)):
         torch.nn.init.xavier_uniform_(m.weight)
 
+snn.reset_state()
+weight_sharing(ann, snn)
+
+snn_q = SNN(batchsize=batchsize)
+snn_q.to(device)
 for _ in range(100):
-    model.train()
+    ann.train()
+    snn.train()
     num_samples = 0
     correct_ann = 0
+    correct_snn = 0
     pbr = tqdm(train_loader)
     for sample, target in pbr:
-        sample = sample.clone().sum(1).sum(1).unsqueeze(1)
-        sample = sample.to(device)
+        # frame data for ann
+        ann_sample = sample.clone().sum(1).sum(1).unsqueeze(1)
+        ann_sample = ann_sample.to(device)
+        # raster data for snn
+        snn_smaple = sample.clone().sum(2).unsqueeze(2)
+        snn_smaple = snn_smaple.to(device)
+
         target = target.to(device)
-        out = model(sample)
-        loss = criterion(out, target)
+
+        # obtain output from ann and snn
+        out_ann = ann(ann_sample)
+        with torch.no_grad():
+            out_snn = snn(snn_smaple)
+
+        _, ann_predict = torch.max(out_ann, 1)
+        _, snn_predict = torch.max(out_snn, 1)
+
+        correct_ann += (ann_predict == target).sum().item()
+        correct_snn += (snn_predict == target).sum().item()
+        num_samples += batchsize
+
+        out_ann.data.copy_(out_snn)
+        loss = criterion(out_ann, target)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        weight_sharing(ann, snn)
+        snn.detach_state()
 
-        _, ann_predict = torch.max(out, 1)
-        correct_ann += (ann_predict == target).sum().item()
-        num_samples += batchsize
+        pbr.set_description(f"ann_train:{round(correct_ann / num_samples, 2)}, snn_train:{round(correct_snn / num_samples, 2)}")
 
-        pbr.set_description(f"acc:{round(correct_ann / num_samples, 4)}")
+    ann.eval()
+    snn.eval()
+    snn_q.eval()
+    weight_sharing(ann, snn_q)
+    _discrete_snn(snn_q.snn)
 
-    model.eval()
-    ann_seq.eval()
-    weight_transfer(model, ann_seq)
     num_samples = 0
     correct_ann = 0
-    correct_Q = 0
-    pbr = tqdm(test_loader)
-
+    correct_snn = 0
+    correct_snnq = 0
     with torch.no_grad():
-        for sample, target in pbr:
-            sample = sample.clone().sum(1).sum(1).unsqueeze(1)
-            sample = sample.to(device)
-            target = target.to(device)
-            out = model(sample)
-            out_Q = ann_seq(sample)
+        pbb = tqdm(test_loader)
+        for sample, target in pbb:
+            ann_sample = sample.clone().sum(1).sum(1).unsqueeze(1)
+            ann_sample = ann_sample.to(device)
+            # raster data for snn
+            snn_smaple = sample.clone().sum(2).unsqueeze(2)
+            snn_smaple = snn_smaple.to(device)
 
-            _, ann_predict = torch.max(out, 1)
-            _, q_predict = torch.max(out_Q, 1)
+            target = target.to(device)
+
+            out_ann = ann(ann_sample)
+            out_snn = snn(snn_smaple)
+            out_snnq = snn_q(snn_smaple)
+
+            _, ann_predict = torch.max(out_ann, 1)
+            _, snn_predict = torch.max(out_snn, 1)
+            _, snnq_predict = torch.max(out_snnq, 1)
 
             correct_ann += (ann_predict == target).sum().item()
-            correct_Q += (q_predict == target).sum().item()
-
+            correct_snn += (snn_predict == target).sum().item()
+            correct_snnq += (snnq_predict == target).sum().item()
             num_samples += batchsize
 
-            pbr.set_description(
-                f"acc:{round(correct_ann / num_samples, 4)}||| Qacc:{round(correct_Q / num_samples, 4)}")
+            snn.detach_state()
+            snn_q.detach_state()
+
+            pbb.set_description(f"ann_test:{round(correct_ann / num_samples, 2)}, snn_test:{round(correct_snn / num_samples, 2)},"
+                                f"snnq_test:{round(correct_snnq / num_samples, 2)}")
