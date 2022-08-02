c531bde04fd6d33f308927abbb377f4972007d87
yannan xing <yannan.xing@synsense.ai>
2022-08-02 12:23:10 +0800

    train with pre-trained cnn with additional term on synops, gradient vanishing not solved,

--- Proxy-learning/eventcim-proxy-learning/main.py
+++ Proxy-learning/eventcim-proxy-learning/main.py
@@ -13,8 +13,15 @@ from typing import Union, Tuple
 from eventcim.network import Network
 from sinabs.layers import IAFSqueeze
 from random import shuffle, seed
+from torch.utils.tensorboard import SummaryWriter
+import time
 
 # ydRGBenv python env
+
+localtime = time.asctime(time.localtime(time.time()))
+tensorboard_dir = f"./tensorboardRC/{localtime}/"
+save_file_name = "simulator-proxy"
+writer = SummaryWriter(f"{tensorboard_dir}{save_file_name}")
 
 number_of_epochs = 50
 
@@ -48,19 +55,26 @@ class ProxyAnnSnn(nn.Module):
             nn.Linear(4 * 4 * 8, 256, bias=False),
             nn.ReLU(),
             nn.Linear(256, 10, bias=False),
-            nn.LeakyReLU()
+            nn.ReLU(),
         )
         self.weight_initializer()
-        self.ann.load_state_dict(torch.load("../nmnist/cnn_10epoch.pth"))
-        self.ann[0].weight.data *= 10
+        self.ann.load_state_dict(torch.load("../nmnist/cnn_10epoch.pth"), strict=False)
+        # self.ann[0].weight.data *= 10
 
     def _generate_simulation_network(self) -> Network:
         # converting the ann to the equivalent snn and deploy the simulator configuration
-        snn = from_model(nn.Sequential(*self.ann[:-1], IAFSqueeze(batch_size=1, spike_threshold=1, min_v_mem=-1)), input_shape=(2, 34, 34)).spiking_model
+        snn = from_model(
+            nn.Sequential(
+                *self.ann[:-1],
+                IAFSqueeze(batch_size=1, spike_threshold=1, min_v_mem=-1),
+            ),
+            input_shape=(2, 34, 34),
+        ).spiking_model
         # snn = from_model(self.ann, input_shape=(2, 34, 34)).spiking_model
         self._reset_snn_state(snn)
         snn = DynapcnnNetwork(snn, discretize=True, input_shape=(2, 34, 34))
         snn = from_dynapcnn_model(snn)
+        a = 1
         return snn
 
     @staticmethod
@@ -79,11 +93,14 @@ class ProxyAnnSnn(nn.Module):
         events = self._xytp_to_events(xytp)
 
         # simulator forward pass
-        snn = self._generate_simulation_network()
 
+        snn = self._generate_simulation_network()
         output_event = snn.forward(events)
+        # output_event = snn.forward_each_layer(events)
         # output evnents --> Tensor
-        snn_output = self._collect_output_events_from_simulator(output_event, number_of_class=10)
+        snn_output = self._collect_output_events_from_simulator(
+            output_event, number_of_class=10
+        )
 
         return ann_output, snn_output
 
@@ -98,11 +115,20 @@ class ProxyAnnSnn(nn.Module):
         return events
 
     @staticmethod
-    def _collect_output_events_from_simulator(output_event_stream: List[Event2d], number_of_class: int) -> Tensor:
+    def _collect_output_events_from_simulator(
+        output_event_stream: List[Event2d], number_of_class: int
+    ) -> Tensor:
         counts = torch.zeros(number_of_class)
         for item in output_event_stream:
             counts[int(str(item).split("\t")[0].split(":")[-1])] += 1
         return counts
+
+    @staticmethod
+    def weight_clipping(models: nn.Sequential, mins=-1, maxs=1):
+        # clipping the weight to be symmetric
+        for lyrs in models:
+            if isinstance(lyrs, nn.Conv2d) or isinstance(lyrs, nn.Linear):
+                lyrs.weight.data.clamp_(min=mins, max=maxs)
 
 
 def get_number_of_correct_samples(network_output: Tensor, label: int) -> int:
@@ -134,7 +160,7 @@ for epochs in range(number_of_epochs):
 
     pbr = tqdm(reading_sequence)
 
-    for read_index in pbr:
+    for step, read_index in enumerate(pbr):
         frame_sample, frame_target = train_frame[read_index]
         xytp_sample, xytp_target = train_xytp[read_index]
 
@@ -144,18 +170,52 @@ for epochs in range(number_of_epochs):
         assert frame_target == xytp_target, "frame target and xytp target is not same!"
 
         out_ann, out_simulator = model.forward(frame=frame_sample, xytp=xytp_sample)
-        print(f"{out_ann}, f{out_simulator}")
+
+        writer.add_scalar("train/ann_output_sum", out_ann.sum(), global_step=step)
+        writer.add_scalar("train/sim_output_sum", out_simulator.sum(), global_step=step)
+
         correct_ann += get_number_of_correct_samples(out_ann, frame_target)
-        correct_simulator += get_number_of_correct_samples(out_simulator.view(1, -1), xytp_target)
+        correct_simulator += get_number_of_correct_samples(
+            out_simulator.view(1, -1), xytp_target
+        )
         number_of_samples += 1
 
         out_ann.data.copy_(out_simulator)
+
+        target_output = 50
+        sim_out_count = out_simulator.sum()
+        out_loss = (1/sim_out_count**2)*np.sqrt((sim_out_count - target_output)**2)
         loss = criterion(out_ann, frame_target)
         running_loss += loss.item()
         optimizer.zero_grad()
         loss.backward()
+
+        for layer_index, ls in enumerate(model.ann):
+            if isinstance(ls, (nn.Conv2d, nn.Linear)):
+                writer.add_scalar(f"lyr{layer_index}/grad_max", ls.weight.grad.data.max(), global_step=step)
+                writer.add_scalar(f"lyr{layer_index}/grad_min", ls.weight.grad.data.min(), global_step=step)
+                writer.add_scalar(f"lyr{layer_index}/grad_average", ls.weight.grad.data.mean(), global_step=step)
         optimizer.step()
 
-        pbr.set_description(f"train_ann:{round(correct_ann / number_of_samples, 4)}, "
-                            f"train_sim{round(correct_simulator / number_of_samples, 4)}", )
-        print(f"sumout:{out_simulator.sum()}")
+        writer.add_scalar(
+            "train/acc_ann", round(correct_ann / number_of_samples, 4), global_step=step
+        )
+        writer.add_scalar(
+            "train/acc_sim",
+            round(correct_simulator / number_of_samples, 4),
+            global_step=step,
+        )
+
+        writer.add_scalar("train/out_loss", out_loss, global_step=step)
+        writer.add_scalar("train/input_count", len(xytp_sample), global_step=step)
+
+        for layer_index, ls in enumerate(model.ann):
+            if isinstance(ls, (nn.Conv2d, nn.Linear)):
+                writer.add_scalar(f"lyr{layer_index}/weight_max", ls.weight.data.max(), global_step=step)
+                writer.add_scalar(f"lyr{layer_index}/weight_min", ls.weight.data.min(), global_step=step)
+                writer.add_scalar(f"lyr{layer_index}/weight_average", ls.weight.data.mean(), global_step=step)
+
+        pbr.set_description(
+            f"train_ann:{round(correct_ann / number_of_samples, 4)}, "
+            f"train_sim{round(correct_simulator / number_of_samples, 4)}",
+        )
